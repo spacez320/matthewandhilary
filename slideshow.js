db3968ad5e5660e424cb8a532a997fc2e3c788f5
Matthew Coleman <matthew.e.coleman@gmail.com>
2025-11-08 09:48:37 -0500

    Slideshow (#3)
    * Slideshow locally
    
    * Publish backend image
    
    * Deploy the backend alongside the frontend
    
    * Custom Nginx route for backend
    
    * Debugging
    
    * Define ingress for backend
    
    * Use new backend address, debugging
    
    * Debugging
    
    * Clean-up

---
+++ slideshow.js
@@ -0,0 +1,30 @@
+var http = require('node:http');
+var fs = require('node:fs');
+
+const server = http.createServer((req, res) => {
+  console.log("Request");
+
+  // Retrieve slideshow images
+  let images = fs.readdirSync('media/images');
+
+  // Allow cross-origin for local development
+  const headers = {
+    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Max-Age': 2592000, // 30 days
+  };
+
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200, haeders);
+    res.end();
+    return
+  }
+
+  headers['Content-Type'] = 'text/json';
+  res.writeHead(200, headers);
+  res.end(JSON.stringify(images));
+
+  console.log("Response");
+})
+
+server.listen(8081, '0.0.0.0', () => {});
