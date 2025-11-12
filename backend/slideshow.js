d148396f01a61f206a86ee723b56c678ab785bce
Matthew Coleman <matthew.e.coleman@gmail.com>
2025-11-11 20:05:30 -0500

    Restructuring (#4)
    * Restructure project
    
    * Local development
    
    * Adjust CI builds, fix routes for prod Kubernetes
    
    * Add Kubernetes deploy to CI

--- backend/slideshow.js
+++ backend/slideshow.js
@@ -1,30 +1,38 @@
 var http = require('node:http');
 var fs = require('node:fs');
 
-const server = http.createServer((req, res) => {
-  console.log("Request");
+process.on('SIGINT', () => {
+  process.exit(0);
+})
 
+process.on('SIGTERM', () => {
+  process.exit(0);
+})
+
+const server = http.createServer((req, res) => {
   // Retrieve slideshow images
   let images = fs.readdirSync('media/images');
 
   // Allow cross-origin for local development
+  //
   const headers = {
-    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Max-Age': 2592000, // 30 days
+    // This defeats the purpose, but isn't relevant any more as requests are never cross-origin
+    // 'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+    // 'Access-Control-Allow-Origin': '*',
+    // 'Access-Control-Max-Age': 2592000, // 30 days
   };
 
+  // Cross-origin requests
   if (req.method === 'OPTIONS') {
     res.writeHead(200, haeders);
     res.end();
     return
   }
 
+  // Normal requests
   headers['Content-Type'] = 'text/json';
   res.writeHead(200, headers);
   res.end(JSON.stringify(images));
-
-  console.log("Response");
 })
 
 server.listen(8081, '0.0.0.0', () => {});
